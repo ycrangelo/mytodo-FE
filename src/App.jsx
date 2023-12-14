3a3875aa4fa574b224f47ee267f0a43beadf9589
ogel <yocorangelo18@gmail.com>
2023-12-14 11:58:25 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import LandingPage from './pages/LandingPage'
+import './App.css'
+import SignupPage from './pages/SignupPage'
+import LoginPage from './pages/SignupPage'
+import Todolist from './pages/Todolist'
+
+function App() {
+
+
+  return (
+    <>
+      <LandingPage /> 
+      {/* <SignupPage />
+      <LoginPage/>
+      <Todolist/> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,40 @@
+
+
+const LandingPage = () => {
+  return (
+    <>
+      <section className="bg-[#fbfdfc]">
+        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+          <div className="mx-auto max-w-xl text-center">
+            <h1 className="text-6xl text-[#060908] font-black sm:text-7xl">
+              TWINKLIN
+            </h1>
+            <h2 className="text-3xl font-black text-[#53b08f] sm:block">Simplify Your Day </h2>
+
+            <p className="mt-4 sm:text-xl/relaxed">
+              Let's Create a Meaningful List of Todos!
+            </p>
+
+            <div className="mt-8 flex flex-wrap justify-center gap-4">
+              <a
+                className="block w-full rounded bg-[#53b08f] px-12 py-3 text-sm font-medium text-[#fbfdfc] shadow hover:bg-[#6bd8b1] focus:outline-none focus:ring active:bg-[#53b08f] sm:w-auto"
+                href="/get-started"
+              >
+                Sign Up
+              </a>
+
+              <a
+                className="block w-full rounded px-12 py-3 text-sm font-medium text-[#53b08f] shadow hover:text-[#6bd8b1] focus:outline-none focus:ring active:text-[#53b08f] sm:w-auto"
+                href="/about"
+              >
+                Login!
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default LandingPage
