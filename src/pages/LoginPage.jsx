5ddeb3c3a6f95b712b3620d6b1df5f475d9756ac
ogel <yocorangelo18@gmail.com>
2023-12-28 19:55:23 +0800

    added some routing again

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -69,7 +69,7 @@ const LoginPage = () => {
                                 <div>
                                     <a href="#"
                                         className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
-                                        <img className="h-5 w-5" src="https://www.svgrepo.com/show/513008/twitter-154.svg"
+                                        <img className="h-5 w-5" src="/public/img/discord.png"
                                             alt="" />
                                     </a>
                                 </div>
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,6 +1,8 @@
 
 
 const SignupPage = () => {
+
+    
     return (
         <>
             <div className="min-h-screen bg-[#fbfdfc] flex flex-col justify-center py-12 sm:px-6 lg:px-8">
@@ -70,7 +72,7 @@ const SignupPage = () => {
                                 <div>
                                     <a href="#"
                                         className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
-                                        <img className="h-5 w-5" src="https://www.svgrepo.com/show/513008/twitter-154.svg"
+                                        <img className="h-5 w-5" src="/public/img/discord.png"
                                             alt="" />
                                     </a>
                                 </div>
