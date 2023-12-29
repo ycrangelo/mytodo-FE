1dbfb0a7c2b0573949a2c0d6156a4894951fbc36
ogel <yocorangelo18@gmail.com>
2023-12-29 11:50:22 +0800

    ayaw ko na

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,70 @@
+import { useState } from "react"
+import axios from "axios"
+import { useEffect } from "react"
 
 
 const LoginPage = () => {
+    const [email, setemail] = useState('')
+    const [password, setPassword] = useState('')
+    const [res,setRes] = useState([])
+
+    const signupUsername = async () => {
+        try {
+            const response = await axios.post(
+                'https://todolist-lbt3.onrender.com/api/user/register',
+                {
+                    email: email,
+                    password: password,
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                       
+                    },
+                }
+            );
+
+            console.log(response);
+             console.log('hello');
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const handlesignupUserName = () => {
+         console.log('hello121');
+        signupUsername()
+        
+    }
+    // const signupDc = () => {
+    //     axios
+    //         .get(`https://todolist-lbt3.onrender.com/api/user/discord`)
+    //         .then((response) => {
+    //             const data = response.data;
+    //             if (Array.isArray(data) && data.length > 0) {
+    //                 setRes(data)
+    //             }
+    //         })
+    //         .catch((error) => {
+    //             console.error('Error fetching data:', error);
+    //         });
+    // }
+
+    // const handlesignupDC = () => {
+      
+    //     signupDc()
+    // }
+    useEffect(() => {
+       
+       handlesignupUserName()
+        
+    }, [handlesignupUserName])
+    // useEffect(() => {
+       
+    //     handlesignupDC()
+        
+    // })
+
     return (
         <>
             <div className="min-h-screen bg-[#fbfdfc] flex flex-col justify-center py-12 sm:px-6 lg:px-8">
@@ -13,13 +77,14 @@ const LoginPage = () => {
 
                 <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md " id="containerLogin">
                     <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                        <form className="space-y-6" action="#" method="POST">
+                        <form className="space-y-6"onClick={ handlesignupUserName()}>
                             <div>
                                 <label htmlFor="email" className="block text-sm font-medium text-gray-700">
                                     Username
                                 </label>
                                 <div className="mt-1">
-                                    <input id="text" name="text" type="text" autoComplete="text" required
+                                    <input id="text" name="text" type='text'
+              value={email}   onChange={(e) => setemail(e.target.value)} autoComplete="text" required
                                         className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                                         placeholder="Enter your username" />
                                 </div>
@@ -30,7 +95,7 @@ const LoginPage = () => {
                                     Password
                                 </label>
                                 <div className="mt-1">
-                                    <input id="password" name="password" type="password" autoComplete="current-password" required
+                                    <input id="password" name="password" type="text"  value={password}   onChange={(e) => setPassword(e.target.value)} autoComplete="current-password" required
                                         className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                                         placeholder="Enter your password" />
                                 </div>
@@ -59,27 +124,9 @@ const LoginPage = () => {
                             </div>
 
                             <div className="mt-6 grid grid-cols-3 gap-3">
-                                <div>
-                                    <a href="#"
-                                        className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
-                                        <img className="h-5 w-5" src="https://www.svgrepo.com/show/512120/facebook-176.svg"
-                                            alt="" />
-                                    </a>
-                                </div>
-                                <div>
-                                    <a href="#"
-                                        className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
-                                        <img className="h-5 w-5" src="/public/img/discord.png"
-                                            alt="" />
-                                    </a>
-                                </div>
-                                <div>
-                                    <a href="#"
-                                        className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
-                                        <img className="h-6 w-6" src="https://www.svgrepo.com/show/506498/google.svg"
-                                            alt="" />
-                                    </a>
-                                </div>
+                                {/* <div onClick={ handlesignupDC()}>
+                                     <img className="w-[2rem]" src="/public/img/discord.png" alt="" onClick={ handlesignupDC()} />
+                               </div> */}
                             </div>
                         </div>
                     </div>
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -72,7 +72,7 @@ const SignupPage = () => {
                                 <div>
                                     <a href="#"
                                         className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
-                                        <img className="h-5 w-5" src="/public/img/discord.png"
+                                        <img className="h-5 w-5" src="/img/discord.png"
                                             alt="" />
                                     </a>
                                 </div>
