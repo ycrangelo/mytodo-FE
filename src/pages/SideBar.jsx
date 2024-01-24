e0eee362c68e35976d262770a34f5150ab502d5a
ogel <yocorangelo18@gmail.com>
2024-01-24 14:50:00 +0800

    added a function add

--- src/pages/SideBar.jsx
+++ src/pages/SideBar.jsx
@@ -27,7 +27,7 @@ const SideBar = () => {
         onClick={handleIsOpen}
         aria-controls="default-sidebar"
         type="button"
-        className="inline-flex items-center p-2 mt-2 text-sm text-gray-500 rounded-lg  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200  dark:focus:ring-gray-600"
+        className="sticky top-0 inline-flex items-center p-2 mt-2 text-sm text-gray-500 rounded-lg  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200  dark:focus:ring-gray-600"
       >
         <span className="sr-only">Open sidebar</span>
         <svg
--- src/pages/Todolist.jsx
+++ src/pages/Todolist.jsx
@@ -1,10 +1,25 @@
 import React, { useState, useEffect } from "react";
 import SideBar from "./SideBar";
+import Add from "./Add";
 
 const Todolist = () => {
    // State to hold the greeting and current time
    const [greeting, setGreeting] = useState("");
    const [currentTime, setCurrentTime] = useState(getFormattedTime());
+   const [imageSrc, setImageSrc] = useState('img/add.png');
+   const [isOpenAdd,setOpenAdd] = useState(false)
+
+  const handleMouseOver = () => {
+    setImageSrc('img/add2.png');
+  };
+
+  const handleMouseOut = () => {
+    setImageSrc('img/add.png');
+   };
+   
+   const handleOpenAdd = () => {
+      setOpenAdd(true)
+   }
 
    // Function to get the current time in the Philippines
    function getFormattedTime() {
@@ -51,59 +66,118 @@ const Todolist = () => {
                <div className="text-center py-11">
                   <h1 className="font-bold text-8xl tracking-wide text-[#3bd38d]">Your todo&apos;s</h1>
                </div>
-               <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                  <div className='border lg:px-7 flex flex-col gap-4'>
+               <div className="grid grid-cols-1 md:grid-cols-1 gap-4 md:mx-[9rem]">
+                  <div className=' lg:px-7 flex flex-col gap-4'>
                      <h1 className="pt-5 font-bold">TITTLE: LITTLE DICK</h1>
                      <p className="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus debitis animi doloremque beatae, quo vitae sequi nam sint voluptatem. Saepe delectus maxime aut pariatur facere consequuntur aliquid beatae hic nihil!</p>
+                     <p className="font-thin">date created:</p>
+                     <div className=" flex justify-end">
+                        <img src="img/delete.png" className="w-[2rem] cursor-pointer" alt="" />
+                     </div>
                   </div>
-                  <div className='border lg:px-7 flex flex-col gap-4'>
+                  <div className=' lg:px-7 flex flex-col gap-4'>
                      <h1 className="pt-5 font-bold">TITTLE: LITTLE DICK</h1>
                      <p className="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi beatae quis fuga dolorum dolorem animi placeat a ratione porro vero!</p>
+                     <p className="font-thin">date created:</p>
+                     <div className=" flex justify-end ">
+                        <img src="img/delete.png" className="w-[2rem] cursor-pointer" alt="" />
+                     </div>
                   </div>
-                  <div className='border lg:px-7 flex flex-col gap-4'>
+                  <div className=' lg:px-7 flex flex-col gap-4'>
                      <h1 className="pt-5 font-bold">TITTLE: LITTLE DICK</h1>
                      <p className="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi beatae quis fuga dolorum dolorem animi placeat a ratione porro vero!</p>
+                     <p className="font-thin">date created:</p>
+                     <div className=" flex justify-end">
+                        <img src="img/delete.png" className="w-[2rem] cursor-pointer" alt="" />
+                     </div>
                   </div>
-                  <div className='border lg:px-7 flex flex-col gap-4'>
+                  <div className=' lg:px-7 flex flex-col gap-4'>
                      <h1 className="pt-5 font-bold">TITTLE: LITTLE DICK</h1>
                      <p className="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi beatae quis fuga dolorum dolorem animi placeat a ratione porro vero!</p>
+                     <p className="font-thin">date created:</p>
+                     <div className=" flex justify-end">
+                        <img src="img/delete.png" className="w-[2rem] cursor-pointer" alt="" />
+                     </div>
                   </div>
-                  <div className='border lg:px-7 flex flex-col gap-4'>
+                  <div className=' lg:px-7 flex flex-col gap-4'>
                      <h1 className="pt-5 font-bold">TITTLE: LITTLE DICK</h1>
                      <p className="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi beatae quis fuga dolorum dolorem animi placeat a ratione porro vero!</p>
+                     <p className="font-thin">date created:</p>
+                     <div className=" flex justify-end">
+                        <img src="img/delete.png" className="w-[2rem] cursor-pointer" alt="" />
+                     </div>
                   </div>
-                  <div className='border lg:px-7 flex flex-col gap-4'>
+                  <div className=' lg:px-7 flex flex-col gap-4'>
                      <h1 className="pt-5 font-bold">TITTLE: LITTLE DICK</h1>
                      <p className="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi beatae quis fuga dolorum dolorem animi placeat a ratione porro vero!</p>
+                     <p className="font-thin">date created:</p>
+                     <div className=" flex justify-end">
+                        <img src="img/delete.png" className="w-[2rem] cursor-pointer" alt="" />
+                     </div>
                   </div>
-                  <div className='border lg:px-7 flex flex-col gap-4'>
+                  <div className=' lg:px-7 flex flex-col gap-4'>
                      <h1 className="pt-5 font-bold">TITTLE: LITTLE DICK</h1>
                      <p className="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi beatae quis fuga dolorum dolorem animi placeat a ratione porro vero!</p>
+                     <p className="font-thin">date created:</p>
+                     <div className=" flex justify-end">
+                        <img src="img/delete.png" className="w-[2rem] cursor-pointer" alt="" />
+                     </div>
                   </div>
-                  <div className='border lg:px-7 flex flex-col gap-4'>
+                  <div className=' lg:px-7 flex flex-col gap-4'>
                      <h1 className="pt-5 font-bold">TITTLE: LITTLE DICK</h1>
                      <p className="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi beatae quis fuga dolorum dolorem animi placeat a ratione porro vero!</p>
+                     <p className="font-thin">date created:</p>
+                     <div className=" flex justify-end">
+                        <img src="img/delete.png" className="w-[2rem] cursor-pointer" alt="" />
+                     </div>
                   </div>
-                  <div className='border lg:px-7 flex flex-col gap-4'>
+                  <div className=' lg:px-7 flex flex-col gap-4'>
                      <h1 className="pt-5 font-bold">TITTLE: LITTLE DICK</h1>
                      <p className="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi beatae quis fuga dolorum dolorem animi placeat a ratione porro vero!</p>
+                     <p className="font-thin">date created:</p>
+                     <div className=" flex justify-end">
+                        <img src="img/delete.png" className="w-[2rem] cursor-pointer" alt="" />
+                     </div>
                   </div>
-                  <div className='border lg:px-7 flex flex-col gap-4'>
+                  <div className=' lg:px-7 flex flex-col gap-4'>
                      <h1 className="pt-5 font-bold">TITTLE: LITTLE DICK</h1>
                      <p className="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi beatae quis fuga dolorum dolorem animi placeat a ratione porro vero!</p>
+                     <p className="font-thin">date created:</p>
+                     <div className=" flex justify-end">
+                        <img src="img/delete.png" className="w-[2rem] cursor-pointer" alt="" />
+                     </div>
                   </div>
-                  <div className='border lg:px-7 flex flex-col gap-4'>
+                  <div className=' lg:px-7 flex flex-col gap-4'>
                      <h1 className="pt-5 font-bold">TITTLE: LITTLE DICK</h1>
                      <p className="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi beatae quis fuga dolorum dolorem animi placeat a ratione porro vero!</p>
+                     <p className="font-thin">date created:</p>
+                     <div className=" flex justify-end">
+                        <img src="img/delete.png" className="w-[2rem] cursor-pointer" alt="" />
+                     </div>
                   </div>
-                  <div className='border lg:px-7 flex flex-col gap-4'>
+                  <div className=' lg:px-7 flex flex-col gap-4'>
                      <h1 className="pt-5 font-bold">TITTLE: LITTLE DICK</h1>
                      <p className="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi beatae quis fuga dolorum dolorem animi placeat a ratione porro vero!</p>
+                     <p className="font-thin">date created:</p>
+                     <div className=" flex justify-end">
+                        <img src="img/delete.png" className="w-[2rem] cursor-pointer" alt="" />
+                     </div>
                   </div>
                </div>
 
             </div>
+          
          </div>
+           <div
+      className="flex justify-end px-3 fixed md:bottom-20 md:right-20 bottom-6 right-1 z-100 transition ease-in-out  hover:-translate-y-1 hover:scale-110 duration-200"
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+    >
+      <img src={imageSrc} alt=""onClick={handleOpenAdd} />
+         </div>
+
+         {isOpenAdd &&  <Add/>}
+        
       </>
    );
 }
