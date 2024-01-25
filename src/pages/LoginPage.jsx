2f2ce017bac72bed1584dcb143d4858e46598404
ogel <yocorangelo18@gmail.com>
2024-01-25 14:53:07 +0800

    onlick HAHHAHAHA

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -87,7 +87,7 @@ const LoginPage = () => {
 
                 <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md " id="containerLogin">
                     <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                        <form className="space-y-6" onClick={(e) =>{ handleloginPage(e)}}
+                        <form className="space-y-6" onSubmit={(e) =>{ handleloginPage(e)}}
                         >
                             <div>
                                 <label htmlFor="email" className="block text-sm font-medium text-gray-700">
