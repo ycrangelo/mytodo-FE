2abb0e97742e2a52c63c1c8edc13f55255b8c4fb
ogel <yocorangelo18@gmail.com>
2024-01-28 01:05:14 +0800

    DONE FINALLY

--- src/pages/Todolist.jsx
+++ src/pages/Todolist.jsx
@@ -8,7 +8,8 @@ import { Card, CardHeader, CardBody, CardFooter, Heading, Button, Text, Flex, Bo
 
 const Todolist = () => {
   const [imageSrc, setImageSrc] = useState('img/add.png');
-  const [todoRes, setTodores] = useState([]);
+   const [todoRes, setTodores] = useState([]);
+   const [isdelete,setdelet] = useState(false)
   const { isLogin, isOpenAdd, setOpenAdd, successfulAdd, setSuccessfullAdd, username, isOK } = useContext(UserContext);
 
   const todosGET = async () => {
@@ -18,7 +19,13 @@ const Todolist = () => {
     } catch (error) {
       console.error('Error fetching user todos:', error);
     }
-  };
+   };
+   const handlesetdelet = () => {
+      setdelet(true)
+   }
+   const handleNotsetdelet = () => {
+      setdelet(false)
+   }
 
   useEffect(() => {
     todosGET();
@@ -45,7 +52,10 @@ const Todolist = () => {
       // Replace 'your-api-endpoint' with the actual API endpoint
       const response = await axios.delete(`https://tasty-sunbonnet-goat.cyclic.app/api/todos/delete/userTodos/${_id}`);
       console.log(response.data); // Handle the response if needed
-      todosGET(); // Refresh the todos after deletion
+       todosGET();
+       if (response) {
+         handlesetdelet()
+      } // Refresh the todos after deletion
     } catch (error) {
       console.error('Error deleting todo:', error);
     }
@@ -53,6 +63,49 @@ const Todolist = () => {
    console.log('this is the list',todoRes)
    return (
       <>
+         {isdelete && <div className="flex justify-center">
+            <div role="alert" className="rounded-xl fixed mt-6  top-0  z-40 border border-gray-100 bg-white p-4">
+               <div className="flex items-start gap-4">
+                  <span className="text-green-600">
+                     <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="h-6 w-6"
+                     >
+                        <path
+                           strokeLinecap="round"
+                           strokeLinejoin="round"
+                           d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                     </svg>
+                  </span>
+
+                  <div className="flex-1">
+                     <strong className="block font-medium text-[#D04848]">Successfully Deleted! </strong>
+
+                   
+                  </div>
+
+                  <button onClick={handleNotsetdelet} className="text-gray-500 transition hover:text-gray-600">
+                     <span className="sr-only">Dismiss popup</span>
+
+                     <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="h-6 w-6"
+                     >
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                     </svg>
+                  </button>
+               </div>
+            </div>
+         </div>}
         
          {successfulAdd && <div className="flex justify-center">
             <div role="alert" className="rounded-xl fixed mt-6  top-0  z-40 border border-gray-100 bg-white p-4">
